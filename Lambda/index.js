c981a8267f78a9bc020ee537faf36fff008e80d5
tpamplin <gartiman1@gmail.com>
2025-04-24 00:40:05 -0400

    change exports to export const

--- Lambda/index.js
+++ Lambda/index.js
@@ -4,7 +4,7 @@ import sharp from "sharp";
 
 const s3 = new S3();
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
     const bucketName = event.Records[0].s3.bucket.name;
     const key = event.Records[0].s3.object.key;
     const filename = key.split("/").pop();
