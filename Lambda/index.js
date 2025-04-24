51ea1c95df38deca267aa265d762606619af3b3f
tpamplin <gartiman1@gmail.com>
2025-04-23 23:01:28 -0400

    add aws sdk

--- Lambda/index.js
+++ Lambda/index.js
@@ -1,1 +1,17 @@
-exports.handler = async (event, context) => {};
+exports.handler = async (event) => {
+    const bucketName = event.Records[0].s3.bucket.name;
+    const key = event.Records[0].s3.object.key;
+    const filename = key.split("/").pop();
+
+    console.log(`Bucket: ${bucketName}`);
+    console.log(`Key: ${key}`);
+    console.log(`Filename: ${filename}`);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "File Processed Successfully.",
+            filename: filename,
+        }),
+    };
+};
